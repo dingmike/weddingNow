da93eff18f9b2abb5539515de0a7463433a3abd8
dingmike <mikezhangding@hotmail.com>
2018-11-17 18:12:25 +0800

    init44robotdf

--- config/weixin.js
+++ config/weixin.js
@@ -1,6 +1,8 @@
 module.exports.weixin = {
   // 微信公众号相关设置
-  appid: 'wx83a881064cf3448a',
-  secret: '927fc61a9555028a6236e90ac7a68e8a',
-  redirectUri: 'http://wedding.tunnel.qydev.com/home/resolve'
+  // appid: 'wx83a881064cf3448a',
+  appid: 'wx2bac5a2f552cd30b',
+  // secret: '927fc61a9555028a6236e90ac7a68e8a',
+  secret: '0921491fd0c34ba2c82a747f046a9f60',
+  redirectUri: 'http://wedding.fecstec.com/home/resolve'
 };
